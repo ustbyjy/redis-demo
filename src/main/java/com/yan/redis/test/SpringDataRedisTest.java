e0bd6920aa8026bcc77457ebaa64c14244183fc4
yanjingyang <yanjingyang@zulong.com>
2017-04-18 20:09:36 +0800

    update

--- src/main/java/com/yan/redis/test/SpringDataRedisTest.java
+++ src/main/java/com/yan/redis/test/SpringDataRedisTest.java
@@ -4,8 +4,11 @@ import com.yan.redis.dao.UserDao;
 import com.yan.redis.entity.User;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
+
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created with IntelliJ IDEA.
@@ -34,5 +37,21 @@ public class SpringDataRedisTest {
         String string1 = stringRedisTemplate.opsForValue().get("redis.string.1");
         System.out.println(string1);
 
+        stringRedisTemplate.opsForValue().set("key111", "v111", 3000, TimeUnit.SECONDS);
+        Long expireTime = stringRedisTemplate.getExpire("key111");
+        System.out.println("expire time: " + expireTime + "s.");
+
+        stringRedisTemplate.opsForSet().add("names", "张三", "李四", "王五");
+        Set<String> nameSet = stringRedisTemplate.opsForSet().members("names");
+        System.out.println(nameSet);
+
+        stringRedisTemplate.opsForList().leftPush("numbers", "1");
+        String number = stringRedisTemplate.opsForList().leftPop("numbers");
+        System.out.println(number);
+        stringRedisTemplate.opsForList().leftPush("numbers", "1");
+        stringRedisTemplate.opsForList().leftPush("numbers", "2");
+        stringRedisTemplate.opsForList().leftPush("numbers", "3");
+        List<String> numberList = stringRedisTemplate.opsForList().range("numbers", 0, 3);
+        System.out.println(numberList);
     }
 }
